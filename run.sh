eefb04e3ea1468a6f1d15f45e9794d775bb16447
Kinrre <enriqueadrian.villarrubia@alu.uclm.es>
2022-05-30 19:33:55 +0200

    feat: run and stop scripts

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Start minikube cluster
+minikube start
+
+# Enable ingress addon
+minikube addons enable ingress
+
+# Set enviroment to minikube docker
+eval $(minikube docker-env)
+
+# Build docker images
+docker build -t pgtic/backend backend
+docker build -t pgtic/frontend frontend
+
+# Create deployments
+kubectl apply -f kubernetes/deployments/frontend-deployment.yml
+kubectl apply -f kubernetes/deployments/backend-deployment.yml
+
+# Create services
+kubectl apply -f kubernetes/services/nginx-service.yml
+kubectl apply -f kubernetes/services/backend-service.yml
+
+# Create ingress
+kubectl apply -f kubernetes/ingress/ingress.yml
+
---
+++ stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Delete ingress
+kubectl delete ingress example-ingress
+
+# Delete services
+kubectl delete services frontend-service
+kubectl delete services backend-service
+
+# Delete deployments
+kubectl delete deployments frontend-deployment
+kubectl delete deployments backend-deployment
+
+# Delete minikube cluster
+minikube delete
+
